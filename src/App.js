031f05e3d87be381f3ecadeb110b7a069f4a5e1e
Ilmir Bakirov <ilmirbakirov@gmail.com>
2021-01-27 17:08:17 +0500

    Revert "update"
    This reverts commit 1fda8989

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import TodoList from "./todos/TodoList";
+
+function App() {
+  return (
+    <div className="app">
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
